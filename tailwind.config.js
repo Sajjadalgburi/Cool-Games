2e881ae412d6692c03920808a2caafa133f2d7bf
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-05 17:07:52 -0400

    feat: Update tailwind.config.js with correct import statements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-import flowbite from "flowbite-react/tailwind";
-import daisyui from "daisyui";
+import flowbite from 'flowbite-react/tailwind';
+import daisyui from 'daisyui';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', flowbite.content()],
   theme: {
     extend: {},
   },
