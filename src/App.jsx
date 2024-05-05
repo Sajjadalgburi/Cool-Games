4c74f28185620fc0afb2f40e2cfef90a58387fda
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-05 17:07:16 -0400

    feat: Add Navigation component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Navigation from './Components/Navbar/Navigation';
 
 function App() {
   return (
     <>
+      <Navigation />
       <Outlet />
     </>
   );
