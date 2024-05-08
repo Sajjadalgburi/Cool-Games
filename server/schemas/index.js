f7edcdb42daacb4f62394e70375ee5f2cf310271
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-08 16:26:34 -0400

    feat: Add server/schemas/index.js to export typeDefs and resolvers

---
+++ server/schemas/index.js
@@ -0,0 +1,6 @@
+// Initiate the typeDefs and resolvers
+const typeDefs = require('./TypeDefs');
+const resolvers = require('./Resolvers');
+
+// Export the typeDefs and resolvers
+module.exports = { typeDefs, resolvers };
