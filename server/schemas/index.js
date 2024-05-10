19e8114b12273ebec44db9392881be0c258f9ddf
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-10 11:57:38 -0400

    refactor: Update import paths for typeDefs and resolvers in index.js

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,6 +1,6 @@
 // Initiate the typeDefs and resolvers
-const typeDefs = require('./TypeDefs');
-const resolvers = require('./Resolvers');
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
 
 // Export the typeDefs and resolvers
 module.exports = { typeDefs, resolvers };
