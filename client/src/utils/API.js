7c0efbb5c2f649c74646ef5ca64f613355bee688
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-10 12:25:31 -0400

    feat: Add API call to get popular games from OpenCritic

---
+++ client/src/utils/API.js
@@ -0,0 +1,32 @@
+// Desc: API call to get popular games from OpenCritic
+
+// Import required libraries
+import axios from 'axios';
+import 'dotenv/config';
+
+// Define a function to get popular games (static - never chaning data )
+const getPopularGames = async () => {
+  try {
+    const options = {
+      method: 'GET',
+      url: 'https://opencritic-api.p.rapidapi.com/game',
+      params: {
+        sort: 'score',
+        skip: '4',
+      },
+      headers: {
+        // Add your RapidAPI key and host to the headers
+        //! (please refer to https://rapidapi.com/opencritic-opencritic-default/api/opencritic-api/pricing to grab an API key)
+        'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+        'X-RapidAPI-Host': 'opencritic-api.p.rapidapi.com',
+      },
+    };
+
+    const response = await axios.request(options);
+    return response.data; // Return the data retrieved from the API
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default getPopularGames;
