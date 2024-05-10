3d69c10fe495913d94d89d5b068c7c877e8954e1
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-10 12:57:03 -0400

    refactor: Update API.js to use dotenv for environment variable loading

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,26 +2,29 @@
 
 // Import required libraries
 import axios from 'axios';
-import 'dotenv/config';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env
+dotenv.config();
+
+const options = {
+  method: 'GET',
+  url: 'https://opencritic-api.p.rapidapi.com/game',
+  params: {
+    sort: 'score',
+    skip: '4',
+  },
+  headers: {
+    // Add your RapidAPI key and host to the headers
+    //! (please refer to https://rapidapi.com/opencritic-opencritic-default/api/opencritic-api/pricing to grab an API key)
+    'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+    'X-RapidAPI-Host': 'opencritic-api.p.rapidapi.com',
+  },
+};
 
 // Define a function to get popular games (static - never chaning data )
 const getPopularGames = async () => {
   try {
-    const options = {
-      method: 'GET',
-      url: 'https://opencritic-api.p.rapidapi.com/game',
-      params: {
-        sort: 'score',
-        skip: '4',
-      },
-      headers: {
-        // Add your RapidAPI key and host to the headers
-        //! (please refer to https://rapidapi.com/opencritic-opencritic-default/api/opencritic-api/pricing to grab an API key)
-        'X-RapidAPI-Key': process.env.RAPID_API_KEY,
-        'X-RapidAPI-Host': 'opencritic-api.p.rapidapi.com',
-      },
-    };
-
     const response = await axios.request(options);
     return response.data; // Return the data retrieved from the API
   } catch (error) {
