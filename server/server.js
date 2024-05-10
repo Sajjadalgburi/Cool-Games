3dd813d28d52f19674a3e1946d2554bf684804c0
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-10 15:43:48 -0400

    refactor: Update server.js to use @apollo/server/express4 middleware

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
-const { start } = require('repl');
+const { expressMiddleware } = require('@apollo/server/express4');
 
 // Define the Express.js application
 const app = express();
@@ -19,3 +19,32 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+
+// Define the startApolloServer function
+const startApolloServer = async () => {
+  await server.start();
+
+  // aplying required middleware for application
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+  app.use('/graphql', expressMiddleware(server, { context: authMiddleware }));
+
+  // serving different files based on the environment
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
+  // Connect to the database and start the Express.js server
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+
+startApolloServer();
