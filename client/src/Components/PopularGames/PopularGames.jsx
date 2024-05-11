32118e3adb6a75b3748804099503f3419337d75d
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-11 09:43:29 -0400

    feat: Update PopularGamesList component to use ApolloClient for fetching popular games
    Refactor PopularGamesList component to use the ApolloClient library for making GraphQL queries to fetch and display popular games. This improves the efficiency and performance of the component by leveraging the power of ApolloClient's caching and network optimization features. Additionally, remove the unused API.js file to clean up the codebase.

--- client/src/Components/PopularGames/PopularGames.jsx
+++ client/src/Components/PopularGames/PopularGames.jsx
@@ -1,62 +1,45 @@
-// Desc: Component to display popular games
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { POPULAR_GAMES_QUERY } from '../../utils/queries';
 
-// importing nessesary libraries
-import { useState, useEffect } from 'react';
-import getPopularGames from '../../utils/API';
-
-// function to display popular games
 const PopularGamesList = () => {
-  // setting the state for games
+  const { loading, error, data } = useQuery(POPULAR_GAMES_QUERY);
   const [games, setGames] = useState([]);
 
-  // function to fetch popular games
   useEffect(() => {
-    const fetchPopularGames = async () => {
-      try {
-        const data = await getPopularGames();
-        setGames(data);
-      } catch (err) {
-        // Handle error (e.g., show error message, log, etc.)
-        console.error('Error fetching games:', err);
-      }
-    };
+    if (!loading && data) {
+      // If data is loaded and there are games, update the state
+      setGames(data.popularGames);
+    }
+  }, [loading, data]); // Trigger effect when loading or data changes
 
-    fetchPopularGames();
-  }, []);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error fetching popular games</p>;
 
   return (
-    <div className="colSystem grid gap-y-8 gap-x-3 grid-cols sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 grid-flow-row-dense">
-      {/* sttyling for each unique card */}
-
+    <div className="colSystem grid gap-y-6 gap-x-4 grid-cols sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 grid-flow-row-dense">
       <h2>Popular Games</h2>
-
-      {/* mapping through the games to display them */}
-
-      {games.map((game) => {
-        return (
-          <div key={game.id} className="card m-5 w-70 bg-base-100 shadow-xl">
-            <figure>
-              <img
-                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-                alt="Shoes"
-              />
-            </figure>
-            <div className="card-body flex flex-col justify-center items-center">
-              {' '}
-              {/* Added flex properties */}
-              <h3 className="card-title">{game.name}</h3>
-              <p>Game Score: {game.topCriticScore}</p>
-              <p>Game Url: {game.url}</p>
-              <div className="card-actions">
-                <button className="btn btn-primary">Take Me!</button>
-              </div>
-            </div>
+      {games.map((game) => (
+        <div key={game.id} className="card w-80 mx-3 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src={`https://opencritic.com/${game.images.banner.sm}`} // Use correct image size here
+              alt={`Image for ${game.name}`}
+            />
+          </figure>
+          <div className="card-body flex flex-col justify-center items-center">
+            <h3 className="card-title">{game.name}</h3>
+            <p>Rating {Math.ceil(game.topCriticScore)}</p>
+            <Link to={game.url}>Game Url</Link>
+            <Link to={'/test'} className="card-actions">
+              <button className="btn btn-primary">View</button>
+            </Link>
           </div>
-        );
-      })}
+        </div>
+      ))}
     </div>
   );
 };
 
-// exporting the component
 export default PopularGamesList;
