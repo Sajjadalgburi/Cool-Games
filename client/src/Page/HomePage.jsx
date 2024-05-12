92da5ffe2468a8fe850f1a0b145689362b423517
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-12 18:49:32 -0400

    feat: Add SearchBar component for implementing search functionality

--- client/src/Page/HomePage.jsx
+++ client/src/Page/HomePage.jsx
@@ -1,49 +1,14 @@
 import { Link } from 'react-router-dom';
 import PopularGamesList from '../Components/PopularGames/PopularGames';
+import SearchBar from '../Components/SearchBar/SearchBar';
 
 const HomePage = () => {
-  const iconStyle = {
-    position: 'absolute',
-    right: '10px', // Adjust the right position of the icon
-    top: '50%', // Center the icon vertically
-    transform: 'translateY(-50%)', // Adjust for vertical centering
-    width: '20px', // Adjust the size of the icon
-    height: '20px',
-    opacity: '0.7',
-    pointerEvents: 'none', // Ensure clicks go through to the input
-  };
-
   return (
     <>
       <div className="grid grid-cols sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-x-6 grid-flow-row p-8">
-        <aside className="">
-          <div style={{ position: 'relative', width: '100%' }}>
-            <input
-              type="text"
-              placeholder="Search for games..."
-              className="input input-bordered"
-              style={{
-                width: '100%',
-                padding: '8px 30px 8px 10px',
-                boxSizing: 'border-box',
-              }}
-            />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              style={iconStyle}
-            >
-              <path
-                fillRule="evenodd"
-                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-
+        <aside>
+          <SearchBar />
           <h2 className="text-3xl font-bold my-5">Categories</h2>
-
           <div className="ListOfCatagories">
             <ul className="Category">
               {' '}
