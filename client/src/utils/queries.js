21e55f774d27811ce45cc2c79a8260ef5360d7cc
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-12 19:24:03 -0400

    Added the QUERY_SINGLE_USER query to fetch a single user's information, including their username and saved games

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,6 @@
 import { gql } from '@apollo/client';
 
+// Define the query for fetching popular games
 export const POPULAR_GAMES_QUERY = gql`
   query PopularGames {
     popularGames {
@@ -9,6 +10,19 @@ export const POPULAR_GAMES_QUERY = gql`
       releaseDate
       link
       image
+    }
+  }
+`;
+
+// Define the query for fetching a single user
+export const QUERY_SINGLE_USER = gql`
+  query singleUser($userId: ID!) {
+    singleUser(userId: $userId) {
+      _id
+      username
+      savedGames {
+        game_id
+      }
     }
   }
 `;
