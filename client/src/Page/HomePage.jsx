3380498b1cf4c6653fd1e91cf72ce66f8c62e933
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-12 18:53:51 -0400

    refactor: Add Catagories component and update HomePage
    Add the Catagories component to display the list of categories on the HomePage. This improves the organization and navigation of the application.

--- client/src/Page/HomePage.jsx
+++ client/src/Page/HomePage.jsx
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
 import PopularGamesList from '../Components/PopularGames/PopularGames';
 import SearchBar from '../Components/SearchBar/SearchBar';
+import Catagories from '../Components/Catagories/Catagories';
 
 const HomePage = () => {
   return (
@@ -8,36 +8,7 @@ const HomePage = () => {
       <div className="grid grid-cols sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-x-6 grid-flow-row p-8">
         <aside>
           <SearchBar />
-          <h2 className="text-3xl font-bold my-5">Categories</h2>
-          <div className="ListOfCatagories">
-            <ul className="Category">
-              {' '}
-              <Link to="/popular">
-                {' '}
-                <li className="mb-4">Popular</li>
-              </Link>
-              <Link to="/shooter">
-                {' '}
-                <li className="mb-4">Shooter</li>
-              </Link>
-              <Link to="/openworld">
-                {' '}
-                <li className="mb-4">Open World</li>
-              </Link>
-              <Link to="/fantasy">
-                {' '}
-                <li className="mb-4">Fantasy</li>
-              </Link>
-              <Link to="/RPB">
-                {' '}
-                <li className="mb-4">RPB</li>
-              </Link>
-              <Link to="/survival">
-                {' '}
-                <li className="mb-4">Survival</li>
-              </Link>
-            </ul>
-          </div>
+          <Catagories />
         </aside>
 
         <main className="ListOfGames col-span-1 md:col-span-4 lg:col-span-5">
