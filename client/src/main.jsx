0aa48025c64f196d5c580442dce5de567db60d33
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-15 20:06:11 -0400

    feat: Add ErrorPage component for displaying unexpected errors

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,12 @@ import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './Page/HomePage.jsx';
 import ProfilePage from './Page/Profile.jsx';
+import ErrorPage from './Page/ErrorPage.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
+    errorElement: <ErrorPage />,
     element: <App />,
     children: [
       {
