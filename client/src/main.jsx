95baaa8015d51219b9187d6482f705834d1e13d0
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-17 16:17:18 -0400

    refactor: Update ProfilePage route to include dynamic user ID parameter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        path: '/profile',
+        path: '/profile/:id',
         element: <ProfilePage />,
       },
     ],
