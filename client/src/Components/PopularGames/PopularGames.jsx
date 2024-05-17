995b899ce2d5f0a03c9e106e1922d5f72a5227eb
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-17 12:32:04 -0400

    refactor: Update PopularGamesList component to handle user authorization before allowing them to click a game

--- client/src/Components/PopularGames/PopularGames.jsx
+++ client/src/Components/PopularGames/PopularGames.jsx
@@ -4,16 +4,26 @@ import { useQuery } from '@apollo/client';
 import { POPULAR_GAMES_QUERY } from '../../utils/queries';
 import { Button } from 'flowbite-react';
 import Auth from '../../utils/auth';
+import { useAuth } from '../../../context/AuthContext';
+import SignUpModal from '../SignUpModal/Index';
+import LoginInModal from '../LoginModal/Index';
 
 const PopularGamesList = () => {
   const { loading, error, data } = useQuery(POPULAR_GAMES_QUERY);
   const [games, setGames] = useState([]);
+  const {
+    currentModal,
+    formState,
+    handleOpenModal,
+    handleCloseModal,
+    handleChange,
+  } = useAuth();
 
   useEffect(() => {
-    if (!loading && data.popularGames) {
+    if (!loading && data && data.popularGames) {
       setGames(data.popularGames);
     }
-  }, [data]);
+  }, [data, loading]);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error fetching popular games</p>;
@@ -21,7 +31,6 @@ const PopularGamesList = () => {
   return (
     <>
       <h2 className="text-4xl font-bold my-9 text-center">Popular Games</h2>
-
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {games.map((game) => (
           <div key={game.game_id} className="border rounded shadow p-4">
@@ -41,13 +50,31 @@ const PopularGamesList = () => {
                 <Button>View Game</Button>
               </Link>
             ) : (
-              <Link className="flex justify-center align-middle">
-                <Button>LOGIN</Button>
-              </Link>
+              <div className="flex justify-center align-middle">
+                <Button onClick={() => handleOpenModal('logIn')}>
+                  View Game
+                </Button>
+              </div>
             )}
           </div>
         ))}
       </div>
+      {currentModal === 'signUp' && (
+        <SignUpModal
+          isOpen={currentModal === 'signUp'}
+          formState={formState}
+          onChange={handleChange}
+          onClose={handleCloseModal}
+        />
+      )}
+      {currentModal === 'logIn' && (
+        <LoginInModal
+          isOpen={currentModal === 'logIn'}
+          formState={formState}
+          onChange={handleChange}
+          onClose={handleCloseModal}
+        />
+      )}
     </>
   );
 };
