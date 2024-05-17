270bb4ab4129d312fbbf32ba72b07938dc4c395e
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-17 18:22:21 -0400

    refactor: Update PopularGamesList component to handle saving games and user authorization

--- client/src/Components/PopularGames/PopularGames.jsx
+++ client/src/Components/PopularGames/PopularGames.jsx
@@ -1,17 +1,20 @@
+// React and React Router imports
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+
+// Apollo Client imports
+import { useQuery, useMutation } from '@apollo/client';
+
+// Utility imports
 import { POPULAR_GAMES_QUERY } from '../../utils/queries';
+import { SAVE_GAME } from '../../utils/mutations';
 import Auth from '../../utils/auth';
+import { saveGameIds, getSavedGameIds } from '../../utils/localStorage';
 import { useAuth } from '../../../context/AuthContext';
+
+// Component imports
 import SignUpModal from '../SignUpModal/Index';
 import LoginInModal from '../LoginModal/Index';
-
-// apollo client
-import { useQuery, useMutation } from '@apollo/client';
-import { SAVE_GAME } from '../../utils/mutations';
-
-// imported the helper functions from localStorage.js
-import { saveGameIds, getSavedGameIds } from '../../utils/localStorage';
 
 const PopularGamesList = () => {
   // use the useQuery hook to make a query request to the server
@@ -35,17 +38,49 @@ const PopularGamesList = () => {
     if (!loading && data && data.popularGames) {
       setGames(data.popularGames);
     }
-
-    // if data exists or has changed from the response of the query, then run the setGames function to update the games state
-    return () => saveGameIds(savedGameIds);
   }, [data, loading]);
+
+  useEffect(() => {
+    return () => saveGameIds(savedGameIds);
+  }, [savedGameIds]);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error fetching popular games</p>;
 
-  const alretUser = () => {
+  const alertUser = () => {
     alert('Please sign up or log in to save a game');
   };
+
+  const [saveGame] = useMutation(SAVE_GAME);
+
+  // function to handle saving a game
+  const handleSaveGame = async (gameId) => {
+    // find the game in `data` by the matching id
+    const gameToSave = games.find((game) => game.game_id === gameId);
+
+    // get token
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const response = await saveGame({
+        variables: { gameData: { ...gameToSave } },
+      });
+
+      // if game successfully saves to user's account, save game id to state
+
+      if (response.data && response.data.saveGame) {
+        setSavedGameIds([...savedGameIds, gameToSave.game_id]);
+      }
+    } catch (err) {
+      // log any error
+      console.error(err);
+    }
+  };
+
   return (
     <>
       <h2 className="text-4xl font-bold my-9 text-center">Popular Games</h2>
@@ -61,16 +96,28 @@ const PopularGamesList = () => {
             <p className="text-gray-600 mb-2">Rating: {game.rating}</p>
             <p className="text-gray-600 mb-2">Genre: {game.genre}</p>
             {Auth.loggedIn() ? (
-              <Link
-                to={`/game/${game.game_id}`}
-                className="flex justify-between align-middle"
-              >
-                <button className="btn btn-secondary">Save Game</button>
-                <button className="btn btn-primary">View Game</button>
-              </Link>
+              <div className="flex justify-between align-middle">
+                <Link to={`/game/${game.game_id}`}>
+                  <button className="btn btn-primary">View Game</button>
+                </Link>
+
+                <button
+                  className="btn btn-secondary"
+                  disabled={savedGameIds?.some(
+                    (savedGameId) => savedGameId === game.game_id,
+                  )}
+                  onClick={() => handleSaveGame(game.game_id)}
+                >
+                  {savedGameIds?.some(
+                    (savedGameId) => savedGameId === game.game_id,
+                  )
+                    ? 'Already Saved'
+                    : 'Save'}
+                </button>
+              </div>
             ) : (
               <div className="flex justify-between align-middle">
-                <button className="btn btn-secondary" onClick={alretUser}>
+                <button className="btn btn-secondary" onClick={alertUser}>
                   Save Game
                 </button>
                 <button
