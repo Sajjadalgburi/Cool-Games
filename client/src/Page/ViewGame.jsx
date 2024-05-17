39324424b8669b0d2a4f5ec01894779279e2db3c
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-17 19:14:27 -0400

    initilized a page to view a single game

---
+++ client/src/Page/ViewGame.jsx
@@ -0,0 +1,9 @@
+// this page is for viewing an individual game through its unique game_id
+
+export default async function ViewSingleGame() {
+  return (
+    <div>
+      <h1>View Game</h1>
+    </div>
+  );
+}
