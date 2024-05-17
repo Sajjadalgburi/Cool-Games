1896c1794cdcbabc21711582e033da298dfa7cfb
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-17 11:34:45 -0400

    refactor: Add AuthContext for managing authentication state to switch between the modals for sign in and login

---
+++ client/context/AuthContext.js
@@ -0,0 +1,48 @@
+import React, { createContext, useContext, useState } from 'react';
+
+// Create a context for authentication
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [currentModal, setCurrentModal] = useState(false);
+  const [formState, setFormState] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleOpenModal = (modalType) => {
+    setFormState({ email: '', password: '', username: '' });
+    setCurrentModal(modalType);
+  };
+
+  const handleCloseModal = () => {
+    setCurrentModal(false);
+    setFormState({
+      username: '',
+      email: '',
+      password: '',
+    });
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState((prev) => ({ ...prev, [name]: value }));
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        currentModal,
+        formState,
+        handleOpenModal,
+        handleCloseModal,
+        handleChange,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
