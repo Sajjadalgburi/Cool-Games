e9483a15d4bb005869df32d05609eb120a479b55
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-19 17:52:12 -0400

    refactor: Update Navigation component to include dropdown menu for navigation links

--- client/src/Components/Navbar/Navigation.jsx
+++ client/src/Components/Navbar/Navigation.jsx
@@ -18,69 +18,134 @@ const Navigation = () => {
 
   return (
     <>
-      <div className="navbar bg-base-100 p-1 mt-2">
+      <div className="navbar bg-base-100 mt-4">
         <div className="navbar-start">
+          <div className="dropdown">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </div>
+
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-2 z-[1] p-3 shadow bg-base-200 rounded-box w-52"
+            >
+              <li>
+                <Link to="/">Homepage</Link>
+              </li>
+
+              {Auth.loggedIn() ? (
+                <>
+                  {' '}
+                  <li>
+                    <Link to={'/'}>
+                      <p onClick={Auth.logout}>logout</p>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="profile">Profile</Link>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li>
+                    <button onClick={() => handleOpenModal('signUp')}>
+                      Sign In
+                    </button>
+                  </li>
+                  <li>
+                    <button onClick={() => handleOpenModal('logIn')}>
+                      Log In
+                    </button>
+                  </li>
+                  {currentModal === 'signUp' && (
+                    <SignUpModal
+                      isOpen={currentModal === 'signUp'}
+                      formState={formState}
+                      onChange={handleChange}
+                      onClose={handleCloseModal}
+                      onClick={handleOpenModal}
+                    />
+                  )}
+                  {currentModal === 'logIn' && (
+                    <LoginInModal
+                      isOpen={currentModal === 'logIn'}
+                      formState={formState}
+                      onChange={handleChange}
+                      onClose={handleCloseModal}
+                    />
+                  )}
+                </>
+              )}
+
+              <li>
+                <Link to="about-me">About me</Link>
+              </li>
+              <li>
+                <Link to="donate">Donate</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-center">
           <Link to="/" className="btn btn-ghost text-2xl uppercase">
             cool games
-          </Link>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu text-lg menu-horizontal px-1 font-bold">
-            <li>
-              <Link to="/">Homepage</Link>
-            </li>
-            <li>
-              <Link to="about-me">About me</Link>
-            </li>
-            {Auth.loggedIn() ? (
-              <>
-                {' '}
-                <li>
-                  <Link to={'/'}>
-                    <p onClick={Auth.logout}>logout</p>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="profile">Profile</Link>
-                </li>
-              </>
-            ) : (
-              <>
-                <li>
-                  <button onClick={() => handleOpenModal('signUp')}>
-                    Sign In
-                  </button>
-                </li>
-                <li>
-                  <button onClick={() => handleOpenModal('logIn')}>
-                    Log In
-                  </button>
-                </li>
-                {currentModal === 'signUp' && (
-                  <SignUpModal
-                    isOpen={currentModal === 'signUp'}
-                    formState={formState}
-                    onChange={handleChange}
-                    onClose={handleCloseModal}
-                    onClick={handleOpenModal}
-                  />
-                )}
-                {currentModal === 'logIn' && (
-                  <LoginInModal
-                    isOpen={currentModal === 'logIn'}
-                    formState={formState}
-                    onChange={handleChange}
-                    onClose={handleCloseModal}
-                  />
-                )}
-              </>
-            )}
-            <li>
-              <Link to="donate">Donate</Link>
-            </li>
-          </ul>
+          </Link>{' '}
         </div>
         <div className="navbar-end">
+          <label className="cursor-pointer grid place-items-center">
+            <input
+              type="checkbox"
+              value="synthwave"
+              className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
+            />
+            <svg
+              className="col-start-1 row-start-1 stroke-base-100 fill-base-100"
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <circle cx="12" cy="12" r="5" />
+              <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+            </svg>
+            <svg
+              className="col-start-2 row-start-1 stroke-base-100 fill-base-100"
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+            </svg>
+          </label>
+
           <Link
             target="_blank"
             rel="noopener noreferrer"
