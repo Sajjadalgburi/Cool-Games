00cc9fcc87d1617c112b182af14442de847c15f0
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-22 12:37:07 -0400

    creating basic landing page for user profile

--- client/src/Page/Profile.jsx
+++ client/src/Page/Profile.jsx
@@ -1,7 +1,20 @@
+import { Link } from 'react-router-dom';
+
 const ProfilePage = () => {
   return (
-    <div>
-      <h1>Profile Page</h1>
+    <div className="profileMain mt-11">
+      <div className="welcomeSection font-bold">
+        <h3 className="text-5xl">Welcome to your profile username!</h3>
+        <p className="mt-8 text-md">
+          Here you can view and manage your saved games.
+        </p>
+
+        <Link to={'/'}>
+          <p className=" text-sm mt-2 italic hover:text-indigo-500 transition">
+            want to go back? Click Me!
+          </p>
+        </Link>
+      </div>
     </div>
   );
 };
