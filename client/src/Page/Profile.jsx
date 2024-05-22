579bac71f2af38cf4f4dbd59c52b10c1daf0a31a
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-22 12:52:42 -0400

    refactor: Update ProfilePage to display saved games count and handle loading state from the currently signed user

--- client/src/Page/Profile.jsx
+++ client/src/Page/Profile.jsx
@@ -1,10 +1,23 @@
 import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_ME } from '../utils/queries';
 
 const ProfilePage = () => {
+  // if data isn't here yet, say so
+  const { loading, data } = useQuery(QUERY_ME);
+
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
+
+  console.log(data.me);
+
   return (
     <div className="profileMain mt-11">
       <div className="welcomeSection font-bold">
-        <h3 className="text-5xl">Welcome to your profile username!</h3>
+        <h3 className="text-5xl">
+          Welcome to your profile {data.me.username}!
+        </h3>
         <p className="mt-8 text-md">
           Here you can view and manage your saved games.
         </p>
@@ -14,6 +27,17 @@ const ProfilePage = () => {
             want to go back? Click Me!
           </p>
         </Link>
+      </div>
+
+      <div className="savedGamesProfile p-4 mt-10">
+        <h5 className="font-semibold text-xl uppercase">
+          {/* Saved Games will be displayed here */}
+          {data.me.savedGames.length
+            ? `Viewing ${data.me.savedGames.length} saved ${
+                data.me.savedGames.length === 1 ? 'game' : 'games'
+              }:`
+            : 'You have no saved games!'}{' '}
+        </h5>
       </div>
     </div>
   );
