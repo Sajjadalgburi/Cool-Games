9b0c37f7d4c369b8938e0009bc97a5e7cbe9ab05
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-24 18:36:16 -0400

    refactor: Update auth.js to use environment variables for secret and expiration

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
-const secret = 'dawjdhajkdhajkwdnaw';
-const expiration = '336h';
+const secret = process.env.SESS_SECRET;
+const expiration = process.env.SESS_LIFETIME;
 
 module.exports = {
   AuthenticationError: new GraphQLError('Could not authenticate user.', {
