ee5255a7c6bfb9a1df1a0d7d78636374e4549388
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-24 18:48:15 -0400

    refactor: Implement lazy loading for game search results in SearchBar component

--- client/src/Components/SearchBar/SearchBar.jsx
+++ client/src/Components/SearchBar/SearchBar.jsx
@@ -1,24 +1,42 @@
+import { useState, useEffect } from 'react';
+import { SINGLE_GAME } from '../../utils/queries';
+import { useLazyQuery } from '@apollo/client';
 // create a functional component SearchBar
-const SearchBar = ({ search, setSearch }) => {
+const SearchBar = () => {
+  const [searchValue, setSearchValue] = useState('');
+
+  const [searchGames, { loading, data, error }] = useLazyQuery(SINGLE_GAME);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    searchGames({ variables: { game: searchValue } });
+  };
+
+  console.log(data);
+
   return (
     <div className="SearchBar">
-      <input
-        type="text"
-        placeholder="Search for games..."
-        className="input input-bordered"
-      />
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 16 16"
-        fill="currentColor"
-        className="search-icon"
-      >
-        <path
-          fillRule="evenodd"
-          d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-          clipRule="evenodd"
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setSearchValue(e.target.value)}
+          placeholder="Search for games..."
+          className="input input-bordered"
         />
-      </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          className="search-icon"
+        >
+          <path
+            fillRule="evenodd"
+            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </form>
     </div>
   );
 };
