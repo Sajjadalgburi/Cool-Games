931efd7c4c89ed9f2d282dd0f130ba92bc61be2b
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-24 17:09:03 -0400

    refactor: Update SearchBar component styles for consistent layout and positioning

--- client/src/Components/SearchBar/SearchBar.jsx
+++ client/src/Components/SearchBar/SearchBar.jsx
@@ -1,33 +1,17 @@
 // create a functional component SearchBar
 const SearchBar = ({ search, setSearch }) => {
-  const iconStyle = {
-    position: 'absolute',
-    right: '10px', // Adjust the right position of the icon
-    top: '50%', // Center the icon vertically
-    transform: 'translateY(-50%)', // Adjust for vertical centering
-    width: '20px', // Adjust the size of the icon
-    height: '20px',
-    opacity: '0.7',
-    pointerEvents: 'none', // Ensure clicks go through to the input
-  };
-
   return (
-    <div style={{ position: 'relative', width: '100%' }}>
+    <div className="SearchBar">
       <input
         type="text"
         placeholder="Search for games..."
         className="input input-bordered"
-        style={{
-          width: '100%',
-          padding: '8px 30px 8px 10px',
-          boxSizing: 'border-box',
-        }}
       />
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 16 16"
         fill="currentColor"
-        style={iconStyle}
+        className="search-icon"
       >
         <path
           fillRule="evenodd"
