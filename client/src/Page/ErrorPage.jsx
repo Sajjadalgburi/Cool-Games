5b40f6f05b4ce79a51059d031e1a6d8b1b622641
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-25 09:31:09 -0400

    refactor: Use navigate hook for going back in ErrorPage component

--- client/src/Page/ErrorPage.jsx
+++ client/src/Page/ErrorPage.jsx
@@ -1,16 +1,17 @@
-import { Link } from 'react-router-dom';
 import { Button } from 'flowbite-react';
+import { useNavigate } from 'react-router-dom';
 
 const ErrorPage = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="error-container">
       <h1 className="text-4xl font-extrabold my-6">OOPS!</h1>
       <p>
         Sorry, an unexpected error has occurred. Click the button to go back!
       </p>
-      <Link to={'/'} className="button-wrapper">
-        <Button>Go Back</Button>
-      </Link>
+      {/* // Add a button to go back using the navigate hook */}
+      <Button onClick={() => navigate(-1)}>Go Back</Button>
     </div>
   );
 };
