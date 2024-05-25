b099093964c8bb8b16375b5ae4d7a9bc0ba8f4df
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-25 11:58:10 -0400

    in this commit i made individual cards for the searched games and added responsiveness as well

--- client/src/Components/SearchResult/SearchResult.jsx
+++ client/src/Components/SearchResult/SearchResult.jsx
@@ -1,13 +1,37 @@
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const SearchResult = () => {
-  const searchData = useSelector((state) => state.search.searchData);
+  const { searchGames } = useSelector((state) => state.search.searchData);
 
   return (
     <div>
-      {searchData ? (
+      <h1 className="flex text-3xl font-bold justify-center mb-6 md:justify-start mt-8 md:ml-24">
+        Results for:{' '}
+      </h1>
+      {searchGames ? (
         <>
-          <p>yesssss</p>
+          <div className="flex justify-center ">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-16 gap-y-8">
+              {searchGames.map((game) => (
+                <div
+                  key={game.game_id}
+                  className="card w-72 bg-base-200 rounded shadow-xl"
+                >
+                  <div className="card-body">
+                    <h2 className="card-title justify-center mb-3">
+                      {game.title}
+                    </h2>
+                    <div className="card-actions justify-center mt-3">
+                      <Link to={`/game/${game.game_id}`}>
+                        <button className="btn btn-primary">Select</button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </>
       ) : (
         <div>
