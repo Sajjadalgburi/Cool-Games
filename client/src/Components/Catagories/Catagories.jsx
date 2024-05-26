bc3ae035b66deffad5595ef4d00801ef59b35add
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-26 14:31:36 -0400

    refactor: Update Catagories component to include additional game categories

--- client/src/Components/Catagories/Catagories.jsx
+++ client/src/Components/Catagories/Catagories.jsx
@@ -9,25 +9,49 @@ const Catagories = () => {
       <div className="ListOfCatagories">
         <ul className="Category">
           {' '}
-          <Link to="/shooter">
+          <Link to="Horror">
             {' '}
-            <li className="mb-4">Shooter</li>
+            <li className="mb-4">Horror</li>
           </Link>
-          <Link to="/openworld">
-            {' '}
-            <li className="mb-4">Open World</li>
-          </Link>
-          <Link to="/fantasy">
+          <Link to="Fantasy">
             {' '}
             <li className="mb-4">Fantasy</li>
           </Link>
-          <Link to="/RPB">
+          <Link to="RPG">
             {' '}
-            <li className="mb-4">RPB</li>
+            <li className="mb-4">RPG</li>
           </Link>
-          <Link to="/survival">
+          <Link to="Survival">
             {' '}
             <li className="mb-4">Survival</li>
+          </Link>
+          <Link to="First-Person Shooter">
+            {' '}
+            <li className="mb-4">First-Person Shooter</li>
+          </Link>
+          <Link to="Racing">
+            {' '}
+            <li className="mb-4">Racing</li>
+          </Link>
+          <Link to="Strategy">
+            {' '}
+            <li className="mb-4">Strategy</li>
+          </Link>
+          <Link to="Art">
+            {' '}
+            <li className="mb-4">Art</li>
+          </Link>
+          <Link to="Interactive Story">
+            {' '}
+            <li className="mb-4">Interactive Story</li>
+          </Link>
+          <Link to="Fighting">
+            {' '}
+            <li className="mb-4">Fighting</li>
+          </Link>
+          <Link to="Simulation">
+            {' '}
+            <li className="mb-4">Simulation</li>
           </Link>
         </ul>
       </div>
