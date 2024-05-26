0e59573f6d28610ca745a1b2833767707ac3b6e3
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-26 18:56:03 -0400

    refactor: Update connection.js to use dotenv for environment variable configuration

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
+
 // Import necessary modules from mongoose
 const { connect, connection } = require('mongoose');
 
-// Establish connection to MongoDB using the provided URI or a default local URI
-connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/coolgames');
+// Establish connection to MongoDB using the provided URI or a default
+connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/coolgames')
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 // Export the connection object to be used elsewhere in the application
 module.exports = connection;
