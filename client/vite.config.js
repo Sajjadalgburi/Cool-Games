b85054517c0fa4c5a6657ea3167bc6e7bf4f8991
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-26 19:31:34 -0400

    refactor: Update build configuration in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   plugins: [react()],
   server: {
     port: 3000,
