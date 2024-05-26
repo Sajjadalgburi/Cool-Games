731ab19e3dac86b3cb13a62c6bf175e55a268bdb
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-26 15:06:08 -0400

    feat: Implement lazy loading for game fethc based on category in CategoryPage

--- client/src/Page/CategoryPage.jsx
+++ client/src/Page/CategoryPage.jsx
@@ -1,4 +1,19 @@
+import { useParams } from 'react-router-dom';
+import { useLazyQuery } from '@apollo/client';
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { CATEGORY_GAME_FETCH } from '../utils/queries';
+
 const CategoryPage = () => {
+  const { categoryName } = useParams();
+
+  const [categoryGameFetch, { loading, data, error }] =
+    useLazyQuery(CATEGORY_GAME_FETCH);
+
+  useEffect(() => {
+    categoryGameFetch({ variables: { category: categoryName } });
+  }, [categoryName, categoryGameFetch]);
+
   return (
     <div>
       <h1>hi</h1>
