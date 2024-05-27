d424f99ffedf9fda668b66d30ed010c327a3e32d
Sajjadalgburi <dfgsajjad@gmail.com>
2024-05-27 15:54:27 -0400

    refactor: Update connection.js to use dotenv for environment variable configuration

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/CoolGames',
-);
+const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/CoolGames';
 
-module.exports = mongoose.connection;
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.error('Connection error:', error);
+});
+
+db.once('open', () => {
+  console.log('Database connection successful');
+});
+
+module.exports = db;
