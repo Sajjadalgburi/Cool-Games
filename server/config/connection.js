0c1d2042b95d54ff522270840fb28ddc2a9c26b2
Sajjadalgburi <dfgsajjad@gmail.com>
2024-06-21 15:49:57 -0600

    fixing mongodb connection 2.0

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,12 @@
-const { connect, connection, mongo } = require('mongoose');
+const { connect, connection } = require('mongoose');
 require('dotenv').config();
 
 const mongoDbUri = process.env.MONGODB_URI;
+const local = 'mongodb://localhost:27017/coolgames';
 
-connect('mongodb://localhost:27017/coolGamesDb' || mongoDbUri);
+// Check if mongoDbUri is defined and use it, otherwise use the local URI
+const uri = mongoDbUri || local;
+
+connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = connection;
